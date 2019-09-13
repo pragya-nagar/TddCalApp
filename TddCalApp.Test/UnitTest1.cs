340f0bd356828cdbfbcaf6623dfe851565d0502a
T-Shirts-Application <pragyanagar99@gmail.com>
2019-09-13 10:40:52 +0530

    Second Test case Passed

--- TddCalApp.Test/UnitTest1.cs
+++ TddCalApp.Test/UnitTest1.cs
@@ -17,7 +17,17 @@ namespace TddCalApp.Test
             result = testCalculator.Add(num1, num2);
             Assert.AreEqual(5, result, "Testing two Integers 3 and 2");
         }
-       
-     
+        [TestMethod]
+        public void ShouldReturnZeroOnPassingOnePositiveAndOneNegative()
+        {
+            Calculator testCalculator = new Calculator();
+            int num1 = -3;
+            int num2 = +3;
+            int result;
+            result = testCalculator.Add(num1, num2);
+            Assert.AreEqual(0, result, "Testing One negative and One positive Integer ");
+        }
+
+
     }
 }
